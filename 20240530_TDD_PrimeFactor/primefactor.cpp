128033022fc7f936a19816fd383d08fb16f1b526
beomsooo.kim <lightjd@naver.com>
2024-05-30 11:36:34 +0900

    [Feature] Cycle1 Green

--- 20240530_TDD_PrimeFactor/primefactor.cpp
+++ 20240530_TDD_PrimeFactor/primefactor.cpp
@@ -1,5 +1,11 @@
-class PrimeFactor
+#include <vector>
+
+class PrimeFactors
 {
 public:
-	
+	std::vector<int> GetPrimeFactors(int n)
+	{
+		std::vector<int> pfs{};
+		return pfs;
+	}
 };
--- 20240530_TDD_PrimeFactor_gtest/primefactor_test.cpp
+++ 20240530_TDD_PrimeFactor_gtest/primefactor_test.cpp
@@ -1,7 +1,11 @@
 #include "pch.h"
 #include "../20240530_TDD_PrimeFactor/primefactor.cpp"
 
-TEST(PrimeFactor, TestOne) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <vector>
+
+TEST(PrimeFactorsTest, Test1) {
+	PrimeFactors pf;
+	int input = 1;
+	std::vector<int> expected{ };
+	EXPECT_EQ(expected, pf.GetPrimeFactors(input));
 }
