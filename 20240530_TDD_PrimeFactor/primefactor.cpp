8a54e9731753714270c73635ee40b37436d89764
beomsooo.kim <lightjd@naver.com>
2024-05-30 12:04:02 +0900

    [Refactoring] Cycle7 Refactor

--- 20240530_TDD_PrimeFactor/primefactor.cpp
+++ 20240530_TDD_PrimeFactor/primefactor.cpp
@@ -6,21 +6,13 @@ public:
 	std::vector<int> GetPrimeFactors(int number)
 	{
 		std::vector<int> pfs;
-		if (number > 1)
+		int divisor = 2;
+		for (divisor = 2; number > 1; divisor++)
 		{
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12)
-			{
-				for (divisor = 2; number > 1; divisor++)
-				{
-					while (number % divisor == 0) {
-						pfs.push_back(divisor);
-						number /= divisor;
-					}
-				}
+			while (number % divisor == 0) {
+				pfs.push_back(divisor);
+				number /= divisor;
 			}
-			else
-				pfs.push_back(number);
 		}
 		return pfs;
 	}
