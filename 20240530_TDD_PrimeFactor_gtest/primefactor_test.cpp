971c8cfc8980b5595b653ccbe420e2023aa4eed1
beomsooo.kim <lightjd@naver.com>
2024-05-30 11:42:07 +0900

    [Refactoring] Cycle2 Refactor

--- 20240530_TDD_PrimeFactor_gtest/primefactor_test.cpp
+++ 20240530_TDD_PrimeFactor_gtest/primefactor_test.cpp
@@ -3,17 +3,20 @@
 
 #include <vector>
 
-TEST(PrimeFactorsTest, Test1) {
+
+class PrimeFactorsFixture : public testing::Test {
+public:
 	PrimeFactors pf;
+	std::vector<int> expected;
+};
+TEST_F(PrimeFactorsFixture, Test1) {
 	int input = 1;
-	std::vector<int> expected{ };
 	EXPECT_EQ(expected, pf.GetPrimeFactors(input));
 }
 
-TEST(PrimeFactorsTest, Test2) {
-	PrimeFactors pf;
+TEST_F(PrimeFactorsFixture, Test2) {
 	int input = 2;
-	std::vector<int> expected{ 2};
+	expected = { 2 };
 	EXPECT_EQ(expected, pf.GetPrimeFactors(input));
 }
 
